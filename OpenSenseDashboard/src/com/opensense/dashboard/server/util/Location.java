d2f52d3bf2ac6cffd8559f2acdac5326c22c7e0c
furny <furny@DESKTOP-PH67D01.Speedport_W_724V_Typ_A_05011603_06_001>
2018-10-30 17:27:40 +0100

    added Location class

---
+++ OpenSenseDashboard/src/com/opensense/dashboard/server/util/Location.java
@@ -0,0 +1,54 @@
+package com.opensense.dashboard.server.util;
+
+
+public class Location { 
+    private final String name;
+    private final double longitude;
+    private final double latitude;   
+   
+    // create and initialize a point with given name and
+    // (latitude, longitude) specified in degrees
+    public Location(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude  = latitude;
+        this.longitude = longitude;
+    }
+
+    // return distance between this location and that location
+    // measured in statute miles
+    public double distanceTo(Location that) {
+        double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+        double lat1 = Math.toRadians(this.getLatitude());
+        double lon1 = Math.toRadians(this.getLongitude());
+        double lat2 = Math.toRadians(that.getLatitude());
+        double lon2 = Math.toRadians(that.getLongitude());
+
+        // great circle distance in radians, using law of cosines formula
+        double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
+                               + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+
+        // each degree on a great circle of Earth is 60 nautical miles
+        double nauticalMiles = 60 * Math.toDegrees(angle);
+        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
+        return statuteMiles;
+    }
+
+    // return string representation of this point
+    public String toString() {
+        return name + " (" + getLatitude() + ", " + getLongitude() + ")";
+    }
+
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+
+	/**
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+}
