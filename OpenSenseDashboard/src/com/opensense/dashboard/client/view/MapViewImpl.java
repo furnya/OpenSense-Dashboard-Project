5ecf6d93944db088b43993bc57397482f26ee8a4
carlroeber98 <carlroeber98@web.de>
2018-11-04 20:30:56 +0100

    added map

--- OpenSenseDashboard/src/com/opensense/dashboard/client/view/MapViewImpl.java
+++ OpenSenseDashboard/src/com/opensense/dashboard/client/view/MapViewImpl.java
@@ -3,6 +3,11 @@ package com.opensense.dashboard.client.view;
 import org.gwtbootstrap3.client.ui.html.Div;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.maps.client.MapImpl;
+import com.google.gwt.maps.client.MapOptions;
+import com.google.gwt.maps.client.MapTypeId;
+import com.google.gwt.maps.client.MapWidget;
+import com.google.gwt.maps.client.base.LatLng;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
@@ -37,11 +42,18 @@ public class MapViewImpl extends DataPanelPageView implements MapView {
 	}
 	
 	private void showThisMap() {
-//		MapOptions mapOptions = MapOptions.newInstance();
-//		mapOptions.setZoom(20);
-//		mapOptions.setCenter(LatLng.newInstance(52.0, 52.0));
-//		MapImpl mapImpl = MapImpl.newInstance(map.getElement(), mapOptions);
-//		MapWidget mapWidget = MapWidget.newInstance(mapImpl);
+		MapOptions mapOptions = MapOptions.newInstance();
+		MapImpl mapImpl = MapImpl.newInstance(map.getElement(), mapOptions);
+		MapWidget mapWidget = MapWidget.newInstance(mapImpl);
+		//Berlin Alexanderplatz coordinates (breitengrad /laengengrad)
+		mapOptions.setCenter(LatLng.newInstance(52.521918,13.413215));
+		mapOptions.setZoom(10);
+		mapOptions.setMapTypeId(MapTypeId.ROADMAP);
+		mapOptions.setDraggable(true);
+		mapOptions.setScaleControl(true);
+		mapOptions.setScrollWheel(true);
+		mapWidget.setVisible(true);
+//		map.add(mapWidget); // die map wird oben schon in den container geladen, wenn du das nochmal versuchst tritt eine exception auf 
 	}
 
 
