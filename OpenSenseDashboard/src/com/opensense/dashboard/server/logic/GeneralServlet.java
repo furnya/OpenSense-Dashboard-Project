d35ebb26bda5f9895cf08e67560068548f1a0902
Lino Steinhau <furnya@gmail.com>
2018-11-05 22:44:24 +0100

    added result class and server call for search view

--- OpenSenseDashboard/src/com/opensense/dashboard/server/logic/GeneralServlet.java
+++ OpenSenseDashboard/src/com/opensense/dashboard/server/logic/GeneralServlet.java
@@ -1,6 +1,7 @@
 package com.opensense.dashboard.server.logic;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -8,6 +9,7 @@ import org.json.JSONObject;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.opensense.dashboard.client.services.GeneralService;
 import com.opensense.dashboard.server.util.RequestSender;
+import com.opensense.dashboard.shared.Result;
 import com.opensense.dashboard.shared.Sensor;
 import com.opensense.dashboard.shared.TestClass;
 
@@ -27,6 +29,29 @@ public class GeneralServlet extends RemoteServiceServlet implements GeneralServi
 			sensorList.add(new Sensor(sensorJSON).getId()+"");
 		}
 		return new TestClass(sensorList);
+	}
+	
+	
+	/**
+	 * @param query the parameters formatted in JSON
+	 * e.g.: {
+	 * 			"maxSensors":100,
+	 * 			"maxAccuracy":10
+	 * 		 }
+	 * @return the list of sensors returned from opensense.network
+	 */
+	public List<Sensor> getSensors(JSONObject query) {
+		requestSender = new RequestSender();
+		for(String key : JSONObject.getNames(query)) {
+			requestSender.addParameter(key, query.getString(key));
+		}
+		JSONArray sensorArray = requestSender.arrayRequest("https://www.opensense.network/progprak/beta/api/v1.0/sensors");
+		LinkedList<Sensor> sensorList = new LinkedList<>();
+		for(Object o : sensorArray) {
+			JSONObject sensorJSON = (JSONObject) o;
+			sensorList.add(new Sensor(sensorJSON));
+		}
+		return sensorList;
 	}
 
 }
